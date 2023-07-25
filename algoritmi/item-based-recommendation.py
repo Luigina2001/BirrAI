ca836b6c164ae648a7c27bc9bd9493699f82cbc2
luigina <l.costante1@studenti.unisa.it>
2023-07-25 17:32:34 +0200

    modificate le funzioni consiglia_ricette_birra e combina_ricette

--- algoritmi/item-based-recommendation.py
+++ algoritmi/item-based-recommendation.py
@@ -12,13 +12,9 @@ dataset = pd.read_csv('../dataset_uniti/ricette.csv')
 pd.set_option('display.max_columns', None)
 
 # selezione dei campi rilevanti per la raccomandazione
-campi_rilevanti = ['category', '[HOPS]_alpha', '[HOPS]_amount', '[HOPS]_time',
-                   '[HOPS]_form', '[HOPS]_name', '[HOPS]_use',
-                   '[FERMENTABLES]_amount', '[FERMENTABLES]_color', '[FERMENTABLES]_yield',
-                   '[FERMENTABLES]_add_after_boil', '[FERMENTABLES]_name', '[FERMENTABLES]_type',
-                   '[YEASTS]_form', '[YEASTS]_name', '[YEASTS]_type', '[MISCS]_amount',
-                   '[MISCS]_amount_is_weight', '[MISCS]_name', '[MISCS]_type',
-                   '[MISCS]_use']
+campi_rilevanti = ['category', '[HOPS]_alpha', '[HOPS]_name',
+                   'est_abv', 'ibu', 'type', '[FERMENTABLES]_color', '[FERMENTABLES]_name',
+                   '[YEASTS]_name', '[YEASTS]_type', '[MISCS]_name']
 
 # encoding numerico che permette di convertire le stringhe in valori numerici per poter essere utilizzati nell'algoritmo
 label_encoders = {}
@@ -40,18 +36,26 @@ def consiglia_ricette_birra(recipe_id, num_recommendations=5):
     # calcola la similarità della ricetta corrente con tutte le altre ricette
     similarita_ricetta = item_similarity[indice_ricetta]
     indici_ordinati = similarita_ricetta.argsort() # indici similarità in ordine crescente
+    indici_ordinati = indici_ordinati[::-1] # indici similarità in ordine decrescente
 
     # seleziono gli indici dalla seconda posizione fino a num_recommendations + 1 (incluso) dagli indici ordinati
     indici_selezionati = indici_ordinati[1:num_recommendations + 1]
 
-    #  inverto l'ordine degli indici selezionati per ottenere gli indici delle ricette più simili in cima alla lista
-    indici_ricette_simili = indici_selezionati[::-1]
+    ricette_consigliate = dataset.loc[indici_selezionati]#, 'name']
 
-    ricette_consigliate = dataset.loc[indici_ricette_simili]#, 'name']
+    # similarità corrispondenti alle ricette consigliate
+    similarita_consigliate = similarita_ricetta[indici_selezionati]
+
+    for i in range(len(ricette_consigliate)):
+        ricetta_nome = ricette_consigliate.iloc[i]['name']
+        similarita = similarita_consigliate[i]
+        print(f"Ricetta consigliata: {ricetta_nome}, Similarità: {similarita}")
+
+    print('\n------------------------------------------------------------------\n')
 
     return ricette_consigliate
 
-def combina_ricette(ricette):
+'''def combina_ricette(ricette):
 
     # combino gli ingredienti o gli attributi delle ricette per creare una nuova ricetta
     nuova_ricetta = {}
@@ -63,25 +67,54 @@ def combina_ricette(ricette):
 
     nuova_ricetta = pd.DataFrame(nuova_ricetta, index=[0])
 
+    return nuova_ricetta'''
+
+def combina_ricette(ricette):
+
+    dataset_originale = pd.read_csv('../dataset_uniti/ricette.csv')
+
+    nuova_ricetta = {
+        'nome': 'Ricetta combinata',
+        'categorie_combinate': [],
+        'Luppoli': [],
+        'Malti': [],
+        'Lieviti': [],
+        'Varie': []
+    }
+
+    # indici delle ricette consigliate
+    indici_ricette_simili = ricette.index
+
+    nuova_ricetta['Luppoli'].extend(dataset_originale.loc[indici_ricette_simili, '[HOPS]_name'].values.flatten().tolist())
+    nuova_ricetta['Malti'].extend(dataset_originale.loc[indici_ricette_simili, '[FERMENTABLES]_name'].values.flatten().tolist())
+    nuova_ricetta['Lieviti'].extend(dataset_originale.loc[indici_ricette_simili, '[YEASTS]_name'].values.flatten().tolist())
+    nuova_ricetta['Varie'].extend(dataset_originale.loc[indici_ricette_simili, '[MISCS]_name'].values.flatten().tolist())
+
+    nuova_ricetta['categorie_combinate'].extend(dataset_originale.loc[indici_ricette_simili, 'category'].values.flatten().tolist())
+
+    # rimuovo gli ingredienti duplicati dalla nuova ricetta
+    nuova_ricetta['Luppoli'] = list(set(nuova_ricetta['Luppoli']))
+    nuova_ricetta['Malti'] = list(set(nuova_ricetta['Malti']))
+    nuova_ricetta['Lieviti'] = list(set(nuova_ricetta['Lieviti']))
+    nuova_ricetta['Varie'] = list(set(nuova_ricetta['Varie']))
+
+    nuova_ricetta['categorie_combinate'] = list(set(nuova_ricetta['categorie_combinate']))
+
     return nuova_ricetta
 
 
-recipe_id = 74302.0     # id di una ricetta presente nel dataset
-ricette_simili = consiglia_ricette_birra(recipe_id, num_recommendations=5)
-
-nuova_ricetta = combina_ricette(ricette_simili)
-
-print('\n------------------------------------------------------------------\n')
-
-print("Ricetta di partenza:")
+recipe_id = 83392.0     # id di una ricetta presente nel dataset
+print("\nRicetta di partenza:")
 print(dataset[dataset['recipe_id'] == recipe_id][['recipe_id', 'name']])
 
 print('\n------------------------------------------------------------------\n')
 
+ricette_simili = consiglia_ricette_birra(recipe_id, num_recommendations=5)
 print(f"Ricette consigliate per la ricetta con id {recipe_id} :")
 print(ricette_simili[['recipe_id', 'name']])
 
 print('\n------------------------------------------------------------------\n')
 
+nuova_ricetta = combina_ricette(ricette_simili)
 print("\nNuova ricetta creata combinando le ricette simili:")
 print(nuova_ricetta)
